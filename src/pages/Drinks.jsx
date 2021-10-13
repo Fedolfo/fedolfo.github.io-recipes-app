ffff15b1adf821ca2a008d24a1d482e0ab73f5ca
Filipe <filipecandido123@hotmail.com>
2021-10-13 15:21:52 -0300

    desabilitando erro do useEffect

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
